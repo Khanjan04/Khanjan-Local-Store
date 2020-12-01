196a2199be1111e8ea3bda56868aa2a965164797
Khanjan <khanjanvarma9@gmail.com>
2020-12-01 23:40:33 +0530

    New Products Added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -23,8 +23,7 @@ function HomeScreen(props) {
                 products.map(product =>
                     <li key={product._id}> 
                         <div className="product">
-                            <Link to={'/product/' + product._id}><img className="product-image" src={product.image} alt="product"/>
-                            </Link>
+                            <Link to={'/product/' + product._id}><img className="product-image" src={product.image} alt="product"/></Link>
                 
                             <div className="product-name">
                             <Link to={'/product/' + product._id}>{product.name}</Link>
