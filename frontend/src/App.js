03c327d3f17a4bfc46cd77fc20f6644945adc48a
Khanjan <khanjanvarma9@gmail.com>
2021-01-18 20:01:02 +0530

    link added to categories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,70 +14,70 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
 
-  const userSignin = useSelector(state=>state.userSignin);
-  const {userInfo} = userSignin;
-  
-  const openMenu = () =>{
-    document.querySelector(".sidebar").classList.add("open");
-  }
-  const closeMenu = () =>{
-    document.querySelector(".sidebar").classList.remove("open");
-  }
+    const userSignin = useSelector(state => state.userSignin);
+    const { userInfo } = userSignin;
 
-  return (
-<BrowserRouter>
+    const openMenu = () => {
+        document.querySelector(".sidebar").classList.add("open");
+    }
+    const closeMenu = () => {
+        document.querySelector(".sidebar").classList.remove("open");
+    }
 
-    <div className="grid-container">
-        <header className="header">
-            <div className="brand">
-                <button onClick={openMenu}>
-                    &#9776;
+    return (
+        <BrowserRouter>
+
+            <div className="grid-container">
+                <header className="header">
+                    <div className="brand">
+                        <button onClick={openMenu}>
+                            &#9776;
                 </button>
-                <Link to="/">Khanjan</Link>
-            </div>
-            <div className="header-links">
-                <a href="cart">Cart</a>
-                {
-                    userInfo ? <Link to="/profile">{userInfo.name}</Link>:
-                    <Link to="/signin">Signin</Link>
-                }
-            </div>
-        </header>
-        <aside className="sidebar">
-            <h3>Shopping Categories</h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-            <ul className="sidebar-list">
-                <li>
-                    <a href="">Pants</a>
-                </li>
-                <li>
-                    <a href="">Shirts</a>
-                </li>
-                <li>
-                    <a href="">Frocks</a>
-                </li>
-            </ul>
-        </aside>
-        <main className="main">
-            <div className="content">
-                <Route path="/products" component={ProductsScreen} />
-                <Route path="/shipping" component={ShippingScreen} />
-                <Route path="/payment" component={PaymentScreen} />
-                <Route path="/placeorder" component={PlaceOrderScreen} />
-                <Route path="/signin" component={SigninScreen} />
-                <Route path="/register" component={RegisterScreen} />
-                <Route path="/product/:id" component={ProductScreen} /> 
-                <Route path="/cart/:id?" component={CartScreen} />
-                <Route path="/" exact={true} component={HomeScreen} />
-                
-            </div>
-            
-        </main>
-        <footer className="footer">
-            All rights are reserved.
+                        <Link to="/">Khanjan</Link>
+                    </div>
+                    <div className="header-links">
+                        <a href="cart">Cart</a>
+                        {
+                            userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                                <Link to="/signin">Signin</Link>
+                        }
+                    </div>
+                </header>
+                <aside className="sidebar">
+                    <h3>Shopping Categories</h3>
+                    <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                    <ul className="sidebar-list">
+                        <li>
+                            <a href="/">Pants</a>
+                        </li>
+                        <li>
+                            <a href="/">Shirts</a>
+                        </li>
+                        <li>
+                            <a href="/">Frocks</a>
+                        </li>
+                    </ul>
+                </aside>
+                <main className="main">
+                    <div className="content">
+                        <Route path="/products" component={ProductsScreen} />
+                        <Route path="/shipping" component={ShippingScreen} />
+                        <Route path="/payment" component={PaymentScreen} />
+                        <Route path="/placeorder" component={PlaceOrderScreen} />
+                        <Route path="/signin" component={SigninScreen} />
+                        <Route path="/register" component={RegisterScreen} />
+                        <Route path="/product/:id" component={ProductScreen} />
+                        <Route path="/cart/:id?" component={CartScreen} />
+                        <Route path="/" exact={true} component={HomeScreen} />
+
+                    </div>
+
+                </main>
+                <footer className="footer">
+                    All rights are reserved.
         </footer>
-    </div>
-    </BrowserRouter>
-  );
+            </div>
+        </BrowserRouter>
+    );
 }
 export default App;
