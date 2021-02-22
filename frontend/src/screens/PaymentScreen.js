e7f6d78623179605ae32df742de9453cd54b2674
Khanjan <khanjanvarma9@gmail.com>
2021-02-22 19:24:27 +0530

    New payment method added

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,44 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { savePayment } from '../actions/cartAction';
 import CheckoutSteps from '../components/CheckoutSteps';
 
 function PaymentScreen(props) {
-    
+
     const [paymentMethod, setPaymentMethod] = useState('');
     const dispatch = useDispatch();
 
-    const submitHandler = (e) =>{
+    const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(savePayment({paymentMethod}));
+        dispatch(savePayment({ paymentMethod }));
         props.history.push('placeorder');
 
     }
     return <div>
         <CheckoutSteps step1 step2 step3 ></CheckoutSteps>
         <div className="form">
-        <form onSubmit={submitHandler} >
-            <ul className="form-container">
-                
-                <li>
-                    <h2>Payment</h2>
-                </li>
-                <li>
-                    <div>
-                    <input type="radio" name="paymentMethod" id="paymentMethod" value="Pay On Delivery" onChange={(e) => setPaymentMethod(e.target.value)}>
-                    </input>
-                    <label htmlFor="paymentMethod">Pay on Delivery</label>
-                    </div>
-                </li>
-                <li>
-                    <button type="submit" className="button primary">Continue</button>
-                </li>
+            <form onSubmit={submitHandler} >
+                <ul className="form-container">
 
-            </ul>
-        </form>
+                    <li>
+                        <h2>Payment</h2>
+                    </li>
+                    <li>
+                        <div>
+                            <input type="radio" name="paymentMethod" id="paymentMethod" value="Pay On Delivery" onChange={(e) => setPaymentMethod(e.target.value)}>
+                            </input>
+                            <label htmlFor="paymentMethod">Cash on Delivery</label>
+                        </div>
+                        <div>
+                            <input type="radio" name="paymentMethod" id="paymentMethod" value="Pay On Delivery" onChange={(e) => setPaymentMethod(e.target.value)}>
+                            </input>
+                            <label htmlFor="paymentMethod">Card on Delivery</label>
+                        </div>
+                    </li>
+                    <li>
+                        <button type="submit" className="button primary">Continue</button>
+                    </li>
+
+                </ul>
+            </form>
         </div>
     </div>
-    
+
 }
 export default PaymentScreen;
