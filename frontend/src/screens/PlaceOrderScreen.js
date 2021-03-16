4cc2bd741690a3dd81b8397a2fdb88be4ddaca05
Khanjan <khanjanvarma9@gmail.com>
2021-03-16 21:53:22 +0530

    Few changes done

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -4,93 +4,93 @@ import { Link } from 'react-router-dom';
 import { addToCart, removeFromCart } from '../actions/cartAction';
 import CheckoutSteps from '../components/CheckoutSteps';
 
-function PlaceOrderScreen(props){
+function PlaceOrderScreen(props) {
 
     const cart = useSelector(state => state.cart);
 
     const { cartItems, shipping, payment } = cart;
-    if(!shipping.address){
+    if (!shipping.address) {
         props.history.push("/shipping");
-    } else if(!payment.paymentMethod){
+    } else if (!payment.paymentMethod) {
         props.location.push("/payment");
     }
-    const itemsPrice = cartItems.reduce((a, c) => a + c.price*c.qty, 0);
-    const shippingPrice = itemsPrice>500 ?0: 50;
-    const taxPrice = 0.18*itemsPrice;
+    const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+    const shippingPrice = itemsPrice > 500 ? 0 : 50;
+    const taxPrice = 0.18 * itemsPrice;
     const totalPrice = itemsPrice + shippingPrice + taxPrice;
 
     const dispatch = useDispatch();
-    
-    const placeOrderHandler = () =>{
+
+    const placeOrderHandler = () => {
         //create an order
     }
-    useEffect(() =>{
-        
+    useEffect(() => {
+
     }, []);
 
-    const checkoutHandler = () =>{
+    const checkoutHandler = () => {
         props.history.push("/signin?redirect=shipping");
     }
 
     return <div>
         <CheckoutSteps step1 step2 step3 step4 ></CheckoutSteps>
         <div className="placeorder">
-        <div className="placeorder-info">
-            <div>
-                <h3>Shipping</h3>
+            <div className="placeorder-info">
                 <div>
-                    {cart.shipping.address}, {cart.shipping.city},
+                    <h3>Shipping</h3>
+                    <div>
+                        {cart.shipping.address}, {cart.shipping.city},
                     {cart.shipping.postalCode}, {cart.shipping.country},
                 </div>
-            </div>
-            <div>
-                <h3>Payment</h3>
-                <div>
-                    Payment Method: {cart.payment.paymentMethod}
                 </div>
-            </div>
-            <div>
-            <ul className="cart-list-container">
-                <li>
-                    <h3>
-                        Shopping Cart
-                    </h3>
+                <div>
+                    <h3>Payment</h3>
                     <div>
-                        Price
+                        Payment Method: {cart.payment.paymentMethod}
                     </div>
-                </li>
-                {
-                    cartItems.length ===0? 
-                    <div>
-                        Cart is empty
-                    </div>
-                    :
-                    cartItems.map( item =>
+                </div>
+                <div>
+                    <ul className="cart-list-container">
                         <li>
-                            <div className="cart-image">
-                            <img src={item.image} alt="product" />
-                            </div>
-                            <div className="cart-name">
-                                <div>
-                                    <Link to={"/product/" + item.product }>
-                                        {item.name}
-                                    </Link>
-                                </div>
-                                <div>
-                                    Qty: {item.qty}
-                                </div>
-                            </div>
-                            <div className="cart-price">
-                                Rs {item.price}
-                            </div>
+                            <h3>
+                                Shopping Cart
+                    </h3>
+                            <div>
+                                Price
+                    </div>
                         </li>
-                        )
-                }
-            </ul>
+                        {
+                            cartItems.length === 0 ?
+                                <div>
+                                    Cart is empty
+                    </div>
+                                :
+                                cartItems.map(item =>
+                                    <li>
+                                        <div className="cart-image">
+                                            <img src={item.image} alt="product" />
+                                        </div>
+                                        <div className="cart-name">
+                                            <div>
+                                                <Link to={"/product/" + item.product}>
+                                                    {item.name}
+                                                </Link>
+                                            </div>
+                                            <div>
+                                                Qty: {item.qty}
+                                            </div>
+                                        </div>
+                                        <div className="cart-price">
+                                            Rs {item.price}
+                                        </div>
+                                    </li>
+                                )
+                        }
+                    </ul>
+                </div>
+
             </div>
-            
-        </div>
-        <div className="placeorder-action">
+            <div className="placeorder-action">
                 <ul>
                     <li>
                         <button className="button primary full-width" onClick={placeOrderHandler} >Place Order</button>
@@ -114,8 +114,8 @@ function PlaceOrderScreen(props){
                         <div>Total</div>
                         <div>Rs {totalPrice}</div>
                     </li>
-                </ul>  
-        </div>
+                </ul>
+            </div>
         </div>
     </div>
 }
