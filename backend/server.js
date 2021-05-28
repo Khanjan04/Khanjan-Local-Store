15b8aa4988296d6f2d1e0d9001b928bf051a19c6
Khanjan <khanjanvarma9@gmail.com>
2021-05-28 17:46:21 +0530

    Changes in server made

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex:true
+    useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
 
@@ -24,11 +24,11 @@ app.use("/api/products", productRoute);
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
-    const product = data.products.find(x=>x._id === productId);
-    if(product)
+    const product = data.products.find(x => x._id === productId);
+    if (product)
         res.send(product);
-    else 
-        res.status(404).send({msg: "Product Not Found." });
+    else
+        res.status(404).send({ msg: "Product Not Found." });
 
 });
 
@@ -36,4 +36,4 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => {console.log("Server started at http://localhost:5000") });
+app.listen(6000, () => { console.log("Server started at http://localhost:6000") });
