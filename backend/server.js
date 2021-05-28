6fd61c9abb375e6e843dd8476b6be920a27a1026
Khanjan <khanjanvarma9@gmail.com>
2021-05-28 20:20:01 +0530

    Change in port

--- backend/server.js
+++ backend/server.js
@@ -36,4 +36,4 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(6000, () => { console.log("Server started at http://localhost:6000") });
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
